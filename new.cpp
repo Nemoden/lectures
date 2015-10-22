647be227336f12ff3c0a8ae3cfce5821b2d6b93f
Kirill "Nemoden" K <i@nemoden.com>
2015-10-22 22:28:28 +1000

    first commit

---
+++ new.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    float u, r;
+    cout << "Enter data: ";
+    cin >> u;
+    cin >> r;
+    cout << "u:" << u << " "   << "r:" <<  r   << endl;
+    return 0; 
+}
